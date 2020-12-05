756a634b56f3f91e8c73360153efb559670e96b3
Ivana Mihalek <ivana.mihalek@gmail.com>
2020-12-05 12:38:04 +0100

    copyright notice

--- chinesevocab/pipeline/mongo_text_component.py
+++ chinesevocab/pipeline/mongo_text_component.py
@@ -1,7 +1,22 @@
-# Define your item pipelines here
+#    Chinesevocab collects several hundred non-generic words related to a specified topic
 #
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#    Copyright (C) 2020 Ivana Mihalek
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Contact: ivana.mihalek@gmail.com
+
 
 # useful for handling different item types with a single interface
 import logging
--- chinesevocab/spiders/extended_topic_vocab_spider.py
+++ chinesevocab/spiders/extended_topic_vocab_spider.py
@@ -1,4 +1,25 @@
 
+
+#    Chinesevocab collects several hundred non-generic words related to a specified topic
+#
+#    Copyright (C) 2020 Ivana Mihalek
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Contact: ivana.mihalek@gmail.com
+
+
 # You can provide command line arguments to your spiders by using the -a option when running them:
 # scrapy crawl extended -a topic=genome
 # These arguments are passed to the Spider’s __init__ method and become spider attributes by default.
